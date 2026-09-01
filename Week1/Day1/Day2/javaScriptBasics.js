b4ec4d2a9bfb4235be23c2d4ce61cef0f649343d
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-09-01 23:28:00 +0530

    Week1Day2

---
+++ Week1/Day1/Day2/javaScriptBasics.js
@@ -0,0 +1,25 @@
+/* Paragraph comment-Shift+Alt+A 
+
+console.log("Step1")
+setTimeout(()=>{console.log("Step2")},3000)
+console.log("Step4") */
+
+// single line comment -> Ctrl+/
+
+/* format the file ->Shift+Alt+F
+console.log("Welcome");
+console.log("to");
+console.log("Playwright"); */
+
+
+/* 
+Naming Convention to be followed as Best practice
+
+Filename and Variable name-> camelCase  ex: javascriptBasics.js
+Class-> PascalCase ex: class LoginPage {}}
+
+ */
+
+
+
+ 
